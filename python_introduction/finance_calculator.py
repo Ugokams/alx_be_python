2d01d9fa138add53d3d9dbac9150624a2a9a4138
Nellydasylva <ugokamslink@gmail.com>
2025-05-22 11:10:21 +0100

    Added float to all users input on financa_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = float(input("Enter your monthly income: "))
 Total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = int(monthly_income - Total_monthly_expenses)
-yearly_savings = int((monthly_savings * 12) + (monthly_savings * 0.05 * 12))
+yearly_savings = ((monthly_savings * 12) + (monthly_savings * 0.05 * 12))
 print (f"your monthly savings are ${monthly_savings}.")
 print (f"Your projected savings after one year, with interest, is: ${yearly_savings}.")
