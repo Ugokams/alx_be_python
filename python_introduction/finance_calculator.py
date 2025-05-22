90f9a21f696e60da57e13c37ad724e3afb6ca410
Nellydasylva <ugokamslink@gmail.com>
2025-05-22 12:29:36 +0100

    Further correction to fix type issues on finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 0.05 * 12)
 print (f"your monthly savings are ${monthly_savings}.")
 print (f"Your projected savings after one year, with interest, is: ${int(projected_savings)}.")
